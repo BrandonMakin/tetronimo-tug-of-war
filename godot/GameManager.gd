4b309a9fd89a940a50a7e0cb0be0a2994cc111fb
Brandon Makin <BrandonMakin@users.noreply.github.com>
2021-12-03 21:18:13 -0800

    rotation works now

--- godot/GameManager.gd
+++ godot/GameManager.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 enum Piece {I, J, L, O, S, T, Z}
 
@@ -40,12 +40,13 @@ func _ready():
 func _process(delta: float) -> void:
 	_do_fall(delta)
 	_do_shift(delta)
+	update()
 	
 func _do_fall(delta: float) -> void:
 	fallingTimer += delta
 	soft_falling = Input.is_action_pressed("soft_drop")
 	if (soft_falling and fallingTimer >= soft_falling_timing_interval) or (!soft_falling and fallingTimer >= falling_timing_interval):
-		_drop_block()
+		_drop_piece()
 		fallingTimer = 0
 
 func _do_shift(delta: float) -> void:
@@ -57,34 +58,43 @@ func _do_shift(delta: float) -> void:
 			var shiftv = Vector2(shift, 0)
 			if !_piece_collides_with_wall(shiftv) && !_piece_collides_with_tiles(shiftv):
 				pos.x += shift
-				_update_block_pos()
+				_update_piece_pos()
 
 func _input(event):
-	if event.is_action_pressed("rotate_cw"):
+	if event.is_action_pressed("rotate_cw") && _piece_can_rotate(piece.Rotation.CLOCKWISE_ONCE):
 		piece.rotate_cw()
-	if event.is_action_pressed("rotate_ccw"):
+	if event.is_action_pressed("rotate_ccw") && _piece_can_rotate(piece.Rotation.COUNTERCLOCKWISE_ONCE):
 		piece.rotate_ccw()
 	if event.is_action_pressed("hard_drop"):
 		_do_hard_drop()
 
-func _update_block_pos() -> void:
+func _update_piece_pos() -> void:
 	piece.position = pos * block_size + block_topleft_offset
 
-func _drop_block() -> void:
-	if _block_can_fall():
+func _drop_piece() -> void:
+	if _piece_can_fall():
 		pos.y += 1
-		_update_block_pos()
+		_update_piece_pos()
 
-func _block_can_fall() -> bool:
-	var bounding_box := piece.current_shape.get_used_rect()
+func _piece_can_fall() -> bool:
 	return !_piece_collides_with_floor(Vector2(0, 1)) && !_piece_collides_with_tiles(Vector2(0,1))
+
+func _piece_can_rotate(rotation) -> bool:
+	for cell in piece.get_cells_after_rotation(rotation):
+		if _tile_collides_with_wall(cell + pos):
+			print("oh?")
+			return false
+		if game_grid.get_cellv(cell + pos) != TileMap.INVALID_CELL:
+			print("woooah")
+			return false
+	return true
 
 func _do_hard_drop() -> void:
 	for _i in range(20):
-		_drop_block()
-	_place_block()
+		_drop_piece()
+	_place_piece()
 
-func _place_block() -> void:
+func _place_piece() -> void:
 	var cells := piece.get_cells()
 	for cell in cells:
 		game_grid.set_cellv(cell + pos, 0)
@@ -98,7 +108,7 @@ func _spawn_next_piece() -> void:
 	piece = pieceScene.instance() as Node2D
 	add_child(piece)
 	pos = starting_pos
-	_update_block_pos()
+	_update_piece_pos()
 
 #func _piece_collides(offset: Vector2) -> bool:
 #	return _piece_collides_with_floor(offset) && \
@@ -122,3 +132,11 @@ func _piece_collides_with_wall(offset: Vector2) -> bool:
 	elif pos.x + offset.x + bounding_box.position.x < 0:
 		return true
 	return false
+
+func _tile_collides_with_wall(point: Vector2) -> bool:
+	return point.x < 0 or point.x > 9
+
+func _draw():
+	for cell in piece.get_cells_after_rotation(piece.Rotation.CLOCKWISE_ONCE):
+		var c = Rect2((cell + pos)*16 + block_topleft_offset, Vector2.ONE*17)
+		draw_rect(c, Color.coral, false, 1)
--- godot/Pieces/Piece.gd
+++ godot/Pieces/Piece.gd
@@ -1,10 +1,25 @@
 extends Node2D
 class_name Piece
 
+enum Orientation {
+	UNSPECIFIED = -1,
+	CLOCKWISE_0 = 0,
+	CLOCKWISE_90 = 1,
+	CLOCKWISE_180 = 2,
+	CLOCKWISE_270 = 4
+}
+
+enum Rotation {
+	NO_ROTATION = 0,
+	CLOCKWISE_ONCE = 1,
+	FLIPPED = 2,
+	COUNTERCLOCKWISE_ONCE = 3
+}
+
 onready var current_shape: TileMap = get_child(0);
 onready var _shape_count = get_child_count()
 
-var _current_shape_index: int = 0
+var _current_shape_index: int = Orientation.CLOCKWISE_0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -27,6 +42,20 @@ func _update_current_shape(shape_index: int) -> void:
 	_current_shape_index = shape_index
 	current_shape = get_child(_current_shape_index)
 	current_shape.visible = true
+
+func _get_shape(orientation) -> TileMap:
+	return get_child(posmod(orientation, _shape_count)) as TileMap
+
+func get_shape_after_rotation(rotation) -> TileMap:
+	return _get_shape(get_orientation_from_rotation(rotation))
 	
-func get_cells() -> Array:
-	return current_shape.get_used_cells()
+func get_cells(orientation := Orientation.UNSPECIFIED) -> Array:
+	if orientation == Orientation.UNSPECIFIED:
+		return current_shape.get_used_cells()
+	return _get_shape(orientation).get_used_cells()
+
+func get_cells_after_rotation(rotation := Rotation.NO_ROTATION) -> Array:
+	return get_cells(get_orientation_from_rotation(rotation))
+
+func get_orientation_from_rotation(rotation):
+	return posmod(_current_shape_index + rotation, _shape_count)
